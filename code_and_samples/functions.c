c746bec7da5ddadda52933bff2a18bfb1c0265bc
ateroh <asger.otte@gmail.com>
2025-09-23 14:55:08 +0200

    basic erosion
    basic erosion. ikke faerdig

--- code_and_samples/functions.c
+++ code_and_samples/functions.c
@@ -55,22 +55,39 @@ void binary_threshold(int threshold, unsigned char input_image[BMP_WIDTH][BMP_HE
 void basic_erosion(int threshold, unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS], unsigned char output_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
     binary_threshold(threshold, input_image, output_image);
 
-    int eroded_cells;
+    int eroded_cells = 1;
+    unsigned char temp_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS];
 
-    
-}
-// Count the number of spots in the image that are not black Step 5
-int detect_spots(unsigned char input_image[BMP_WIDTH][BMP_HEIGTH][BMP_CHANNELS]) {
-    int pixel_count = 0;
-    for (int i = 0; i < BMP_WIDTH; i++) {
-        for (int j = 0; j < BMP_HEIGTH; j++) {
+    //Creating a temporaray 3d array so that we can run multiple erosions
+    for (int x = 0; x < BMP_WIDTH; x++) {
+        for (int y = 0; y < BMP_HEIGTH; y++) {
             for (int c = 0; c < BMP_CHANNELS; c++) {
-                if (input_image[i][j][c] == 255) {
-                    pixel_count++;
+                temp_image[x][y][c] = output_image[x][y][c];
+            }
+        }   
+    }
+    while (eroded_cells) {
+        eroded_cells = 0; // incase of only one erosion needed
+        for (int x = 1; x < BMP_WIDTH-1; x++) {         // we go from 1 to width-1 to avoid borders 
+            for (int y = 1; y < BMP_HEIGTH-1; y++) {
+                if (temp_image[x][y][2] == 255 
+                    && temp_image[x-1][y][2] == 255 
+                    && temp_image[x+1][y][2] == 255 
+                    && temp_image[x][y-1][2] == 255 
+                    && temp_image[x][y+1][2] == 255) {    
+                    // checks all orthogonally adjacent cell to see if they are also white. If so, do nothing (keep alive)
+                } else {
+                    for (int c = 0; c < BMP_CHANNELS; c++) {
+                        output_image[x][y][c] = 0;   
+
+                        }
+                        eroded_cells = 1;   // cells were eroded
+                    }
                 }
             }
-        }
-    }
 
-    return pixel_count;
+    }
+    
+
+
 }
